441346304368e2daf12d7e9110d882af183bbd79
tjkellymagellanhealth <tjkelly@magellanhealth.com>
2019-11-26 10:45:56 -0600

    src main java

---
+++ FruitLib/FruitLib/src/main/java/Fruit.java
@@ -0,0 +1,17 @@
+
+
+class Fruit {
+	String name;
+	
+	static Fruit fruit(String fruitName) {
+		Fruit f = new Fruit();
+		f.name =fruitName;
+		return f;
+	}
+
+	@Override
+	public String toString() {
+		return "Fruit [name=" + name + "]";
+	}
+	
+}
