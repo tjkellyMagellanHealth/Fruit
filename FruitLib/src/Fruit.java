6addd43fba4734a56efc3c74cc923891826f7e56
tjkellymagellanhealth <tjkelly@magellanhealth.com>
2019-11-26 10:21:34 -0600

    init

---
+++ FruitLib/src/Fruit.java
@@ -0,0 +1,39 @@
+
+
+/** Version 2.0*/
+class Fruit {
+	/** @since Version 2.0 */
+	String key;
+	/** @since Version 1.0 */
+	String name;
+	/** @since Version 2.0 */
+	@Override
+	public boolean equals(Object that) {
+		if(that instanceof Fruit) {
+			Fruit thatFruit = (Fruit) that;
+			return this.key.equals ( thatFruit.key);
+		}
+		return false;
+	}
+
+	/** @since Version 1.0 
+	 *  modified in version 2.0 */
+	@Override
+	public String toString() {
+		return "Fruit [key=" + key + ", fruitName=" + name + "]";
+	}
+	/** @since Version 1.0 */
+	@Deprecated
+	static Fruit fruit(String fruitName) {
+		Fruit f = new Fruit();
+		f.name =fruitName;
+		return f;
+	}
+	/** @since Version 2.0 */
+	static Fruit fruit(String key, String fruitName) {
+		Fruit f = new Fruit();
+		f.key = key;
+		f.name =fruitName;
+		return f;
+	}
+}
